b2e89e29c0a43e179be4e2d43adcd6d6029a553b
Jaborg <jlappinsadiq@gmail.com>
2023-10-10 00:08:18 +0100

    Setting up Airflow dag

---
+++ airflow/dags/kafka_workflow.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'me',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'kafka_workflow',
+    default_args=default_args,
+    description='A simple Kafka producer-consumer workflow',
+    schedule_interval=timedelta(minutes=30),
+    start_date=datetime(2023, 10, 9),
+    catchup=False,
+)
+
+# Assume producer.py and consumer.py are in the ~/airflow/dags/ folder or provide the full path
+produce_task = BashOperator(
+    task_id='produce_to_kafka',
+    bash_command='python3 producer.py',
+    dag=dag,
+)
+
+consume_task = BashOperator(
+    task_id='consume_from_kafka',
+    bash_command='python3 consumer.py',
+    dag=dag,
+)
+
+produce_task >> consume_task
